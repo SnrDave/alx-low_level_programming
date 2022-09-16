fd470f28353b6312797c52d187d20956b1e84a29
SnrDave <davewaelson@gmail.com>
2022-09-16 00:57:29 -0700

    2nd

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
-*
-*Return: returns 0
+* main - prints the numbers from 1 to 100
+* 3 multiples print Fizz instead of the number
+* 5 multiples print Buzz instead of the number
+* 3 & 5 multiples print FizzBuzz instead of the number
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int num = 1;
+int i;
 
-while (num++ < 100)
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
+for (i = 1; i <= 100; i++)
 {
-if ((num % 3 == 0) && (num % 5 == 0))
-{
-printf("FizzBuzz ");
-}
-else if ((num % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((num % 5) == 0)
-{
-if (num != 100)
-{
-printf("Buzz ");
-}
+if (i == 100)
+printf("%s", b);
+else if ((i % 3 == 0) && (i % 5 == 0))
+printf("%s ", fb);
+else if (i % 3 == 0)
+printf("%s ", f);
+else if (i % 5 == 0)
+printf("%s ", b);
 else
-{
-printf("Buzz");
-}
-}
-else
-{
-printf("%d ", num);
-}
+printf("%d ", i);
 }
 printf("\n");
 return (0);
