8b4c5145ad254db7747f1bcd2b992a2dbb54ad0d
SnrDave <davewaelson@gmail.com>
2022-10-04 04:31:06 -0700

    2nd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,9 +6,11 @@
 * @argv: argument that stores the strings in an array of char* (strings)
 * Return: 0
 */
+
 int main(int argc, char *argv[])
 {
 int num_1, num_2, mul;
+
 if (argc != 3)
 printf("Error\n");
 else
