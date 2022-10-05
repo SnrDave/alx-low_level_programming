cd47ffd6ca21296e51925945ea33d1b228e06773
SnrDave <davewaelson@gmail.com>
2022-10-05 06:46:57 -0700

    dw

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+/**
+* str_concat -> string concatinating function
+* @s1: string 1
+* @s2: string 2
+* Return: string 1 + string 2
+*/
 char *str_concat(char *s1, char *s2)
 {
-int i, j, k;
-char *newStr = NULL;
+int i = 0, j = 0, l = 0, k = 0;
+char *s;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i] != '\0'; i++);
-for (j = 0; s2[j] != '\0'; j++);
-newStr = (char*)malloc((i + j) * sizeof(char));
-if (newStr == NULL)
+while (s1[i])
+i++;
+while (s2[j])
+j++;
+l = i + j;
+s = (char *)malloc(l *sizeof(char) + 1);
+if (s == NULL)
 return (NULL);
-for (k = 0; s1[k] != '\0'; k++)
-newStr[k] = s1[k];
-for (k = 0; s2[k] != '\0'; k++)
-newStr[k + i] = s2[k];
-return (newStr);
+j = 0;
+while (k < l)
+{
+if (k < i)
+s[k] = s1[k];
+if (k >= i)
+{
+s[k] = s2[j];
+j++;
+}
+k++;
+}
+s[k] = '\0';
+return (s);
 }
