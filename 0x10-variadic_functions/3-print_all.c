daee073e67808778fa805ea3ee4133a8d67237db
SnrDave <davewaelson@gmail.com>
2022-10-15 03:44:01 -0700

    dw

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,21 @@
 #include "variadic_functions.h"
-
 #include <stdlib.h>
-
 #include <stdio.h>
-
-
-
 /**
-
- * _printchar - print char type element from va_list
-
- * @list: va_list passed to function
-
- */
-
+* _printchar - print char type element from va_list
+* @list: va_list passed to function
+*/
 void _printchar(va_list list)
-
 {
-
 	printf("%c", va_arg(list, int));
-
 }
-
-
-
 /**
-
- * _printstr - print string element from va_list
-
- * @list: va_list passed to function
-
- */
-
+* _printstr - print string element from va_list
+* @list: va_list passed to function
+*/
 void _printstr(va_list list)
-
 {
-
 	char *s;
-
-
 
 	s = va_arg(list, char *);
 
@@ -47,57 +24,28 @@ void _printstr(va_list list)
 		s = "(nil)";
 
 	printf("%s", s);
-
 }
-
-
-
 /**
-
- * _printfloat - print float type element from va_list
-
- * @list: va_list passed to function
-
- */
-
+* _printfloat - print float type element from va_list
+* @list: va_list passed to function
+*/
 void _printfloat(va_list list)
-
 {
-
 	printf("%f", va_arg(list, double));
-
 }
-
-
-
 /**
-
- * _printint - print int type element from va_list
-
- * @list: va_list passed to function
-
- */
-
+* _printint - print int type element from va_list
+* @list: va_list passed to function
+*/
 void _printint(va_list list)
-
 {
-
 	printf("%d", va_arg(list, int));
-
 }
-
-
-
 /**
-
- * print_all - print anything passed if char, int, float, or string.
-
- * @format: string of formats to use and print
-
- */
-
+* print_all - print anything passed if char, int, float, or string.
+* @format: string of formats to use and print
+*/
 void print_all(const char * const format, ...)
-
 {
 
 	unsigned int i, j;
